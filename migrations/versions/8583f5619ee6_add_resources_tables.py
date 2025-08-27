e2cf7202d289b54acbffecf3cb32ea96489c04d3
Batdog <carl.ackermann@gmail.com>
2025-08-27 14:45:24 -0600

    Fix resources migration and clean up

--- migrations/versions/8583f5619ee6_add_resources_tables.py
+++ migrations/versions/8583f5619ee6_add_resources_tables.py
@@ -4,21 +4,32 @@ Revision ID: 8583f5619ee6
 Revises: 0030_location_notes
 Create Date: 2025-08-27 20:16:11.331844
 """
+
 from alembic import op
 import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = "8583f5619ee6"
+down_revision = "0030_location_notes"
+branch_labels = None
+depends_on = None
 
 
 def upgrade() -> None:
     op.create_table(
         "resources",
         sa.Column("id", sa.Integer, primary_key=True),
-        sa.Column("name", sa.String(255), nullable=False),
-        sa.Column("type", sa.String(20), nullable=False),
+        sa.Column("name", sa.String(255), nullable=False, unique=True),
+        sa.Column("type", sa.String(16), nullable=False),
         sa.Column("resource_value", sa.String(2048)),
         sa.Column(
             "active", sa.Boolean, nullable=False, server_default=sa.text("true")
         ),
-        sa.Column("created_at", sa.DateTime, server_default=sa.func.now()),
+        sa.Column(
+            "created_at",
+            sa.DateTime(timezone=True),
+            server_default=sa.text("timezone('utc', now())"),
+        ),
     )
     op.create_table(
         "resource_workshop_types",
@@ -33,11 +44,6 @@ def upgrade() -> None:
             sa.Integer,
             sa.ForeignKey("workshop_types.id", ondelete="CASCADE"),
             primary_key=True,
-        ),
-        sa.UniqueConstraint(
-            "resource_id",
-            "workshop_type_id",
-            name="uix_resource_workshop_type",
         ),
     )
 
